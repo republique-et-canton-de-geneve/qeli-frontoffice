1db132f407a67afcb4c3f2616ed45e06864fa4c2
Boix Requesens Josep (DI) <josep.boixrequesens@etat.ge.ch>
2020-01-16 11:57:24 +0100

    [QELI-28] Question 0401 : Quelle est votre nationalité ?

---
+++ qeli-frontoffice-ihm/src/app/core/question/nationalite-question/nationalite-question.component.ts
@@ -0,0 +1,70 @@
+import {
+  AfterViewInit, Component, ElementRef, Input, OnDestroy, QueryList, ViewChild, ViewChildren
+} from '@angular/core';
+import { RegisterQuestionComponent } from '../question-registry';
+import { QuestionComponent } from '../question.component';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { NationaliteQuestion } from './nationalite-question.model';
+
+@RegisterQuestionComponent(new NationaliteQuestion().controlType)
+@Component({
+  selector: 'app-nationalite-question',
+  templateUrl: './nationalite-question.component.html',
+  styleUrls: ['./nationalite-question.component.scss']
+})
+export class NationaliteQuestionComponent implements QuestionComponent<any>, AfterViewInit {
+  @Input() question: NationaliteQuestion;
+  @Input() form: FormGroup;
+
+  @ViewChild('checkboxApatride') checkboxApatride: ElementRef;
+  @ViewChildren('selectPays') selectPays: QueryList<ElementRef>;
+
+  numberOfNationalites = 1;
+  maxNumberOfNaationalites = 3;
+
+  ngAfterViewInit() {
+    this.selectPays.toArray()[0].nativeElement.focus();
+  }
+
+  get isApatride() {
+    return this.form.value[this.question.key]['apatride'];
+  }
+
+  onApatrideChanged() {
+    if (this.isApatride) {
+      this.paysArray.disable();
+    } else {
+      this.paysArray.enable();
+    }
+  }
+
+  private get paysArray() {
+    const nationaliteControl = (this.form.controls[this.question.key] as FormGroup);
+    return nationaliteControl.controls['pays'] as FormArray;
+  }
+
+  popOrClearPaysControl() {
+    if (this.numberOfNationalites === 1) {
+      this.paysArray.setValue([null]);
+    } else {
+      this.paysArray.removeAt(this.paysArray.length - 1);
+      this.numberOfNationalites -= 1;
+    }
+  }
+
+  addPaysControl() {
+    if (this.numberOfNationalites < this.maxNumberOfNaationalites) {
+      this.paysArray.push(new FormControl());
+      this.numberOfNationalites += 1;
+    }
+  }
+
+  hasPays() {
+    return (this.paysArray.value as []).filter(e => e !== null).length > 0;
+  }
+
+  hasUnsetPays() {
+    return (this.paysArray.value as []).filter(e => e === null).length > 0;
+  }
+
+}
