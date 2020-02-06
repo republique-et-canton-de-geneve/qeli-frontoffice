505b6d8a2950918fee2c4ac57873df9974650c4e
Wujkowski Guillaume (DI) <guillaume.wujkowski@etat.ge.ch>
2020-02-06 13:46:07 +0100

    [QELI-104] correction du chemin vers le csv

--- qeli-frontoffice-cypress/cypress/integration/common/FormulaireCSV.js
+++ qeli-frontoffice-cypress/cypress/integration/common/FormulaireCSV.js
@@ -1,12 +1,11 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
-
 const neatCSV = require('neat-csv');
 
 let donnees;
 let scenarios = [];
 
 Given("j'utilise le jeu de données {string}", (fichier) => {
-  cy.readFile('cypress/integration/features-metier/csv/' + fichier)
+  cy.readFile('cypress/integration/features-metier/' + fichier)
     .then(neatCSV) // converts text into list of objects
     /* eslint-disable-next-line no-console */
     .then(console.table) // convenient method for printing list of objects in DevTools console
@@ -21,29 +20,29 @@ Given("j'utilise le jeu de données {string}", (fichier) => {
 });
 
 Then('je contrôle mes scenarii sur le formulaire', () => {
-  scenarios.forEach((scenario) => {
-    cy.connectionFormulaire();
+    scenarios.forEach((scenario) => {
+      cy.connectionFormulaire();
 
-    donnees.forEach((row) => {
-      let valeur = row[scenario];
-      let clef = row["question"];
+        donnees.forEach((row) => {
+          let valeur = row[scenario];
+          let clef = row["question"];
 
-      if (valeur && valeur.trim() !== '') {
-        switch (clef) {
-          case Cypress.env('PRESTATIONS_ELIGIBLES'):
-            cy.isPrestationEligible(valeur);
-            break;
-          case Cypress.env('PRESTATIONS_REFUSEES'):
-            cy.isPrestationRefusee(valeur);
-            break;
-          case Cypress.env('PRESTATIONS_PERCUES'):
-            cy.isPrestationPercue(valeur);
-            break;
-          default:
-            cy.answerQuestion(clef, valeur, true);
-            break;
-        }
-      }
+          if (valeur && valeur.trim() !== '') {
+            switch (clef) {
+              case Cypress.env('PRESTATIONS_ELIGIBLES'):
+                cy.isPrestationEligible(valeur);
+                break;
+              case Cypress.env('PRESTATIONS_REFUSEES'):
+                cy.isPrestationRefusee(valeur);
+                break;
+              case Cypress.env('PRESTATIONS_PERCUES'):
+                cy.isPrestationPercue(valeur);
+                break;
+              default:
+                cy.answerQuestion(clef, valeur, true);
+                break;
+            }
+          }
+        });
+      });
     });
-  });
-});
