b013d778dbb04d640750ac2fd0a649b3510c2920
Boix Requesens Josep (DI) <josep.boixrequesens@etat.ge.ch>
2020-02-10 13:38:10 +0100

    [QELI-107] Credentials pour DEV

--- qeli-frontoffice-application/src/distrib/dev-deploy.sh
+++ qeli-frontoffice-application/src/distrib/dev-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd ***REMOVED***/
-ln -f -s ./livraison`ls -rt livraison/ | tail -n1` webapps/socialqeli_pub
+ln -f -s ../livraison/`ls -rt livraison/ | tail -n1` webapps/socialqeli_pub.war
 sh ./etc/tomcat.bash restart
