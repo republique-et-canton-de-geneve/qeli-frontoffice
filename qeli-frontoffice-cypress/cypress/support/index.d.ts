23fcdfcf0a8c95a3ccd9b1f3b544b80b084f3764
Boix Requesens Josep (DI) <josep.boixrequesens@etat.ge.ch>
2020-02-28 18:45:41 +0100

    [QELI-36] Modifications des commandes cypress

--- qeli-frontoffice-cypress/cypress/support/index.d.ts
+++ qeli-frontoffice-cypress/cypress/support/index.d.ts
@@ -5,22 +5,24 @@ declare namespace Cypress {
 
     /**
      * Récupère un élément depuis son attribut data-cy
+     *
      * @param value Valeur de l'attribut data-cy
-     * @example
-     * cy.dataCy('0101_prestations')
+     *
+     * @example cy.dataCy('0101_prestations')
      */
     dataCy(value: string): Chainable<any>
 
     /**
      * Récupère un élément depuis son attribut data-cy-label
+     *
      * @param value Valeur de l'attribut data-cy-label
-     * @example
-     * cy.dataCyLabel('0301_etatCivil')
+     * @example cy.dataCyLabel('0301_etatCivil')
      */
     dataCyLabel(value: string): Chainable<any>
 
     /**
      * Récupère un élément depuis son attribut data-cy-${eligibilite}=${prestation}
+     *
      * @param eligibilite Suffixe de l'attribut data-cy-
      * @param prestation Valeur de l'attribut data-cy-*
      */
@@ -28,7 +30,9 @@ declare namespace Cypress {
 
     /**
      * Vérifie l'affichage du résultat d'une ou plusieurs prestations par état (éligible, perçue, refusée)
-     * @param statut Statut de l'éligibilitée
+     *
+     * @param statut Statut de l'éligibilitée, valeurs possibles : prestations-eligibles, prestations-refusees ou
+     *   prestations-percues
      * @param prestation Prestation(s) testée(s)
      */
     checkStatutPrestation(statut: string, prestation: string): Chainable<any>
@@ -53,8 +57,8 @@ declare namespace Cypress {
      *
      * @param question couple 'code + key' de la question, exemple : "0101_prestations"
      * @param answer réponse à la question, peut être une liste de valeurs séparée par une virgule
-     * @example
-     * cy.answerQuestion('0401_nationalite', 'ch,be')
+     *
+     * @example cy.answerQuestion('0401_nationalite', 'ch,be')
      */
     answerNationalite(question: string, answer: string): Chainable<any>
 
@@ -65,8 +69,8 @@ declare namespace Cypress {
      * @param question Couple 'code + key' de la question, exemple : "0101_prestations"
      * @param answer Réponse à la question, peut être une liste de valeurs séparée par une virgule pour les checkbox
      * @param validate Valide ou non la question courante pour passer à la suivante (bouton suivant)
-     * @example
-     * cy.answerQuestion('0101_prestations', 'SUBSIDES', false)
+     *
+     * @example cy.answerQuestion('0101_prestations', 'SUBSIDES', false)
      */
     answerQuestion(question: string, answer: string, validate: boolean): Chainable<any>
 
