e669e4f8c1bade3b0d59e19a7ff4ac2af57f6e6f
Boix Requesens Josep (DI) <josep.boixrequesens@etat.ge.ch>
2020-03-03 14:59:47 +0100

    [QELI-105] Corrections de fautes de frappes

--- qeli-frontoffice-ihm/src/app/home/model/revenus.model.ts
+++ qeli-frontoffice-ihm/src/app/home/model/revenus.model.ts
@@ -2,7 +2,7 @@ export enum TypeRevenus {
   EMPLOI                = "EMPLOI",
   CHOMAGE               = "CHOMAGE",
   AVS_RETRAITE          = "AVS_RETRAITE",
-  AVS_VOEUF             = "AVS_VOEUF",
+  AVS_VEUF             = "AVS_VEUF",
   AVS_ENFANT            = "AVS_ENFANT",
   AI_INVALIDITE         = "AI_INVALIDITE",
   AI_ENFANT             = "AI_ENFANT",
