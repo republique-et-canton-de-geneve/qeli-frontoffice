a6b858119857943804a4fd8fbad4b359d798362e
Boix Requesens Josep (DI) <josep.boixrequesens@etat.ge.ch>
2020-03-03 15:02:31 +0100

    [QELI-105] Utiliser APG pour les indemnités journalière

--- qeli-frontoffice-ihm/src/app/home/model/revenus.model.ts
+++ qeli-frontoffice-ihm/src/app/home/model/revenus.model.ts
@@ -6,5 +6,5 @@ export enum TypeRevenus {
   AVS_ENFANT            = "AVS_ENFANT",
   AI_INVALIDITE         = "AI_INVALIDITE",
   AI_ENFANT             = "AI_ENFANT",
-  INDEMNITE_JOURNALIERE = "INDEMNITE_JOURNALIERE"
+  APG = "APG"
 }
