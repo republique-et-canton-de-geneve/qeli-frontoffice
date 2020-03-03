e1ee1bb59b89baed27a8cf4f8655824b934affb4
Boix Requesens Josep (DI) <josep.boixrequesens@etat.ge.ch>
2020-03-03 09:49:14 +0100

    [QELI-105] Modifications de la question activité

---
+++ qeli-frontoffice-ihm/src/app/home/model/revenus.model.ts
@@ -0,0 +1,10 @@
+export enum TypeRevenus {
+  EMPLOI                = "EMPLOI",
+  CHOMAGE               = "CHOMAGE",
+  AVS_RETRAITE          = "AVS_RETRAITE",
+  AVS_VOEUF             = "AVS_VOEUF",
+  AVS_ENFANT            = "AVS_ENFANT",
+  AI_INVALIDITE         = "AI_INVALIDITE",
+  AI_ENFANT             = "AI_ENFANT",
+  INDEMNITE_JOURNALIERE = "INDEMNITE_JOURNALIERE"
+}
