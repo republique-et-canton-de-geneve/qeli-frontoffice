5756ce3a96cf21dacd4e9a003f6bf73fd5ec0627
ouillonke <ouillon.ko.kevin@etat.ge.ch>
2020-03-30 01:35:26 +0200

    [QELI-33]: ajout endpoint generation pdf

--- qeli-frontoffice-rest/src/main/java/ch/ge/social/qeli/api/formulaire/Formulaire.java
+++ qeli-frontoffice-rest/src/main/java/ch/ge/social/qeli/api/formulaire/Formulaire.java
@@ -1,19 +1,21 @@
 package ch.ge.social.qeli.api.formulaire;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonRootName(value = "documentContent")
 public class Formulaire {
 
-  List<Answer> answers;
+    List<Answer> answers;
 
-  List<String> prestationDejaPercues;
+    List<String> prestationDejaPercues;
 
-  List<String> prestationEligible;
+    List<String> prestationEligible;
 
-  List<Refus> prestationRefusees;
+    List<Refus> prestationRefusees;
 
 }
--- qeli-frontoffice-rest/src/main/java/ch/ge/social/qeli/api/formulaire/jaxb/Metier.java
+++ qeli-frontoffice-rest/src/main/java/ch/ge/social/qeli/api/formulaire/jaxb/Metier.java
@@ -1,17 +1,22 @@
-/*package ch.ge.social.qeli.api.formulaire.jaxb;
+package ch.ge.social.qeli.api.formulaire.jaxb;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
 
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"texte1"})
 public class Metier {
+
+  public Metier(){}
 
   public Metier(String aText1){
     this.texte1 = aText1;
   }
 
-  @XmlAttribute
+  @XmlElement
   private String texte1;
 
-}*/
+}
