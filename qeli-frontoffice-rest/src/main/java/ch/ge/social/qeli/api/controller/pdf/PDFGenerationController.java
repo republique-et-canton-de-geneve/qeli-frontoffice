48602e5c4d9dccb3954512273f6343b20b54a4e9
Josep Boix Requesens <josep.boixrequesens@etat.ge.ch>
2020-04-07 14:06:43 +0200

    [QELI-33] Connection avec l'editique

--- qeli-frontoffice-rest/src/main/java/ch/ge/social/qeli/api/controller/pdf/PDFGenerationController.java
+++ qeli-frontoffice-rest/src/main/java/ch/ge/social/qeli/api/controller/pdf/PDFGenerationController.java
@@ -3,12 +3,8 @@ package ch.ge.social.qeli.api.controller.pdf;
 import ch.ge.social.qeli.service.api.pdf.PDFCreationService;
 import ch.ge.social.qeli.service.api.pdf.PDFGenerationException;
 import ch.ge.social.qeli.service.api.pdf.dto.QeliResult;
-import com.google.common.io.ByteStreams;
 import java.io.IOException;
-import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,13 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PDFGenerationController {
 
   private final PDFCreationService pdfCreationService;
-  private final Resource           src;
 
   @Autowired
-  public PDFGenerationController(PDFCreationService pdfCreationService,
-                                 @Value("classpath:/hbt.pdf") Resource src) {
+  public PDFGenerationController(PDFCreationService pdfCreationService) {
     this.pdfCreationService = pdfCreationService;
-    this.src = src;
   }
 
   /**
@@ -43,10 +36,7 @@ public class PDFGenerationController {
    */
   @PostMapping(value = "/pdf", consumes = {MediaType.APPLICATION_JSON_VALUE})
   public byte[] generatePDF(@RequestBody QeliResult result) throws IOException {
-    // TODO return this.pdfCreationService.generatePDF(form);
-    try (InputStream input = src.getInputStream()) {
-      return ByteStreams.toByteArray(input);
-    }
+    return this.pdfCreationService.generate(result);
   }
 
 
