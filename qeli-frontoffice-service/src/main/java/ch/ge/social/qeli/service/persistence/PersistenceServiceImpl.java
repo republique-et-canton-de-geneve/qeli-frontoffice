4bfad2626cc75a42626915e34beb5abd13ac5a17
ouillonke <ouillon.ko.kevin@etat.ge.ch>
2020-04-08 10:56:56 +0200

    [QELI-187]: persiste logs

--- qeli-frontoffice-service/src/main/java/ch/ge/social/qeli/service/persistence/PersistenceServiceImpl.java
+++ qeli-frontoffice-service/src/main/java/ch/ge/social/qeli/service/persistence/PersistenceServiceImpl.java
@@ -13,7 +13,7 @@ import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.   beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
