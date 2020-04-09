251ad295bef5d4ef574eed7d8f68f5d768d5a0b0
ouillonke <ouillon.ko.kevin@etat.ge.ch>
2020-04-09 09:22:40 +0200

    [QELI-176]: revenu conjoint

--- qeli-frontoffice-ihm/src/app/service/question/revenus/revenus-question.service.ts
+++ qeli-frontoffice-ihm/src/app/service/question/revenus/revenus-question.service.ts
@@ -2,8 +2,15 @@ import { Injectable } from '@angular/core';
 import { QuestionLoader } from '../question-loader';
 import { QeliConfiguration } from '../../configuration/qeli-configuration.model';
 import { TypeRevenus } from './revenus.model';
-import { QeliQuestionDecorator } from '../qeli-question-decorator.model';
-import { Eligibilite } from '../eligibilite.model';
+import { Categorie, QeliQuestionDecorator, Subcategorie } from '../qeli-question-decorator.model';
+import { Eligibilite, EligibiliteGroup, EligibiliteRefusee } from '../eligibilite.model';
+import {
+  CheckboxGroup,
+  CheckboxGroupAnswer, CheckboxGroupQuestion
+} from '../../../dynamic-question/checkbox-group-question/checkbox-group-question.model';
+import { Demandeur, MembreFamille, Relation } from '../../configuration/demandeur.model';
+import { FormData, QuestionOption } from '../../../dynamic-question/model/question.model';
+import { Prestation } from '../../configuration/prestation.model';
 
 const REVENUS_OPTIONS = [
   {label: TypeRevenus.EMPLOI},
@@ -32,7 +39,96 @@ const REVENUS_OPTIONS = [
 export class RevenusQuestionService implements QuestionLoader {
 
   loadQuestions(configuration: QeliConfiguration, eligibilites: Eligibilite[]): QeliQuestionDecorator<any>[] {
-    return [/*
+
+    const eligibiliteGroup = new EligibiliteGroup(eligibilites);
+    const membres = ([eligibiliteGroup.demandeur] as (MembreFamille | Demandeur)[]).concat(
+      eligibiliteGroup.demandeur.membresFamille.filter(
+        membre => membre.relation === Relation.PARTENAIRE_ENREGISTRE ||
+                  membre.relation === Relation.EPOUX ||
+                  membre.relation === Relation.CONCUBIN
+      )
+    );
+
+    return membres.map((membre): QeliQuestionDecorator<any>[] => {
+      const translateParams = {who: membre.id === 0 ? 'me' : 'them', membre: membre.prenom};
+      return [
+        {
+          question: new CheckboxGroupQuestion({
+            key: `revenusConjoint_${membre.id}`,
+            dataCyIdentifier: `0602_revenusConjoint_${membre.id}`,
+            label: {
+              key: 'queston.revenusConjoint.label'
+            },
+            noneOptions: [
+              {
+                value: 'OUI',
+                label: {
+                  key: 'question.revenusConjoint.none'
+                }
+              },
+              {
+                value: 'NON',
+                label: {
+                  key: 'question.revenusConjoint.some'
+                }
+              }
+            ],
+            checkboxOptions: REVENUS_OPTIONS.map(revenuOption => {
+              const options = !!revenuOption.options ? revenuOption.options.map(option => { return {value: option.label, label: {key: `question.revenusConjoint.option.${option.label}`}} as QuestionOption<string>; }) : null;
+              return !!options ? {label: {key: `question.revenusConjoint.option.${revenuOption.label}`}, options: options } as CheckboxGroup : {label: {key: ''} } as CheckboxGroup;
+            })
+          }),
+          calculateRefus: this.calculateRevenusRefusFn(membre),
+          eligibilites: eligibiliteGroup.findByPrestationEtMembre([Prestation.PC_FAM,
+                                                                   Prestation.AIDE_SOCIALE,
+                                                                   Prestation.PC_AVS_AI,
+                                                                   Prestation.BOURSES], membre),
+          categorie: Categorie.SITUATION_PERSONELLE,
+          subcategorie: Subcategorie.REVENUS
+        }
+      ];
+    }).reduce((result, current) => result.concat(current), []);
+  }
+
+  calculateRevenusRefusFn(membre: MembreFamille | Demandeur) {
+    return (formData: FormData, eligibilites: Eligibilite[]): EligibiliteRefusee[] => {
+      const refus: EligibiliteRefusee[] = null;
+      const revenusAnswer = formData['prestations'] as CheckboxGroupAnswer;
+      const choices = revenusAnswer.choices;
+
+      if (choices.some(choice => choice.value === 'AVS' || choice.value === 'AI')) {
+        this.createRefusByPrestation(
+          [Prestation.PC_FAM, Prestation.AIDE_SOCIALE], refus, eligibilites
+        ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
+      }
+
+      if (choices.some(choice => choice.value === 'CHOMAGE' || choice.value === 'AVS_RETRAITE' || choice.value === 'AI_INVALIDITE')) {
+        this.createRefusByPrestation(
+          [Prestation.PC_FAM, Prestation.AIDE_SOCIALE], refus, eligibilites
+        ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
+      }
+
+      return [];
+    };
+  }
+
+  //TODO : Duplicate from prestation-question.service -> maybe refactor to a question.util ?
+  private createRefusByPrestation(prestations: Prestation[],
+                                  refus: EligibiliteRefusee[],
+                                  eligibilites: Eligibilite[]) {
+    return eligibilites.filter(eligibilite => prestations.includes(eligibilite.prestation))
+                       .filter(eligibilite => !this.isEligibiliteRefusee(refus, eligibilite))
+                       .map(eligibilite => ({
+                         eligibilite: eligibilite,
+                         motif: {key: `question.revenusConjoint.motifRefus.${eligibilite.prestation}`}
+                       } as EligibiliteRefusee));
+  }
+
+  private isEligibiliteRefusee(refus: EligibiliteRefusee[], eligibilite: Eligibilite) {
+    return new EligibiliteGroup(refus.map(refus => refus.eligibilite)).includes(eligibilite);
+  }
+
+      /*
       new CheckboxGroupQuestion({
         key: 'revenus',
         code: '0601',
@@ -189,6 +285,5 @@ export class RevenusQuestionService implements QuestionLoader {
           }
         ]
       })*/
-    ];
-  }
+
 }
