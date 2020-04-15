aa9e8bcedf46f5d8fa6c19f311d03bca278bb3fc
ouillonke <ouillon.ko.kevin@etat.ge.ch>
2020-04-15 15:32:11 +0200

    [QELI-176]: revenus conjoint et enfants

--- qeli-frontoffice-ihm/src/app/service/question/revenus/revenus-question.service.ts
+++ qeli-frontoffice-ihm/src/app/service/question/revenus/revenus-question.service.ts
@@ -10,28 +10,6 @@ import {
 import { Demandeur, MembreFamille, Relation } from '../../configuration/demandeur.model';
 import { FormData, QuestionOption } from '../../../dynamic-question/model/question.model';
 import { Prestation } from '../../configuration/prestation.model';
-import { OptionAnswer, StringAnswer } from '../../../dynamic-question/model/answer.model';
-
-const REVENUS_OPTIONS = [
-  {label: TypeRevenus.EMPLOI},
-  {label: TypeRevenus.CHOMAGE},
-  {
-    label: 'AVS', options: [
-      {label: TypeRevenus.AVS_RETRAITE},
-      {label: TypeRevenus.AVS_VEUF},
-      {label: TypeRevenus.AVS_ENFANT}
-    ]
-  },
-
-  {
-    label: 'AI', options: [
-      {label: TypeRevenus.AI_ENFANT},
-      {label: TypeRevenus.AI_INVALIDITE}
-    ]
-  },
-  {label: TypeRevenus.APG, help: true}
-];
-
 
 @Injectable({
   providedIn: 'root'
@@ -42,12 +20,7 @@ export class RevenusQuestionService implements QuestionLoader {
 
     const eligibiliteGroup = new EligibiliteGroup(eligibilites);
     const membres = ([eligibiliteGroup.demandeur] as (MembreFamille | Demandeur)[]).concat(
-      eligibiliteGroup.demandeur.membresFamille.filter(
-        membre => membre.relation === Relation.PARTENAIRE_ENREGISTRE ||
-                  membre.relation === Relation.EPOUX ||
-                  membre.relation === Relation.CONCUBIN ||
-                  membre.relation === Relation.ENFANT
-      )
+      eligibiliteGroup.demandeur.membresFamille
     );
 
      return membres.map((membre): QeliQuestionDecorator<any>[] => {
@@ -55,87 +28,36 @@ export class RevenusQuestionService implements QuestionLoader {
       return [
         {
           question: new CheckboxGroupQuestion({
-            key: `revenusConjoint_${membre.id}`,
-            dataCyIdentifier: `0602_revenusConjoint_${membre.id}`,
+            key: `revenus_${membre.id}`,
+            dataCyIdentifier: `0602_revenus_${membre.id}`,
             label: {
-              key: 'question.revenusConjoint.label'
-            },
-            noneOptions: [
-              {
-                value: 'OUI',
-                label: {
-                  key: 'question.revenusConjoint.none'
-                }
-              },
-              {
-                value: 'NON',
-                label: {
-                  key: 'question.revenusConjoint.some'
-                }
-              },
-              {value: 'INCONNU', label: {key: 'question.prestations.inconnu'}}
-            ],
-            checkboxOptions: REVENUS_OPTIONS.map(revenuOption => {
-              const options = !!revenuOption.options
-                ? revenuOption.options.map(option => { return {value: option.label, label: {key: `question.revenusConjoint.option.${option.label}`}} as QuestionOption<string>; })
-                : null;
-
-              const text = !!revenuOption.help
-                ? {key: `question.revenusConjoint.option.${revenuOption.label}.label`, help: `question.revenusConjoint.option.${revenuOption.label}.help`}
-                : {key: `question.revenusConjoint.option.${revenuOption.label}`};
-
-              return !!options
-                ? {label: text, options: options} as CheckboxGroup
-                : {label: text, value: revenuOption.label } as QuestionOption<string>;
-            })
-          }),
-          calculateRefus: this.calculateRevenusRefusFn(membre),
-          eligibilites: eligibiliteGroup.findByPrestationEtMembre([Prestation.PC_FAM,
-                                                                   Prestation.AIDE_SOCIALE,
-                                                                   Prestation.PC_AVS_AI,
-                                                                   Prestation.BOURSES], membre),
-          categorie: Categorie.SITUATION_PERSONELLE,
-          subcategorie: Subcategorie.REVENUS
-        },
-        {
-          question: new CheckboxGroupQuestion({
-            key: `revenusEnfants_${membre.id}`,
-            dataCyIdentifier: `0602_revenusEnfants_${membre.id}`,
-            label: {
-              key: 'question.revenusEnfants.label',
+              key: 'question.revenus.label',
               parameters: translateParams
             },
             noneOptions: [
               {
                 value: 'OUI',
                 label: {
-                  key: 'question.revenusEnfants.none'
+                  key: 'question.revenus.none',
+                  parameters: translateParams
                 }
               },
               {
                 value: 'NON',
                 label: {
-                  key: 'question.revenusEnfants.some'
+                  key: 'question.revenus.some',
+                  parameters: translateParams
                 }
               },
-              {value: 'INCONNU', label: {key: 'question.prestations.inconnu'}}
+              {value: 'INCONNU', label: {key: 'question.revenus.inconnu'}}
             ],
-            checkboxOptions: REVENUS_OPTIONS.map(revenuOption => {
-              const options = !!revenuOption.options
-                              ? revenuOption.options.map(option => { return {value: option.label, label: {key: `question.revenusEnfants.option.${option.label}`}} as QuestionOption<string>; })
-                              : null;
-
-              const text = !!revenuOption.help
-                           ? {key: `question.revenusEnfants.option.${revenuOption.label}.label`, help: `question.revenusEnfants.option.${revenuOption.label}.help`}
-                           : {key: `question.revenusEnfants.option.${revenuOption.label}`};
-
-              return !!options
-                     ? {label: text, options: options} as CheckboxGroup
-                     : {label: text, value: revenuOption.label } as QuestionOption<string>;
-            })
+            checkboxOptions: this.refusOptions(translateParams)
           }),
-          calculateRefus: this.calculateRevenusRefusEnfantsFn(membre),
-          eligibilites: eligibiliteGroup.findByPrestationEtMembre([Prestation.PC_AVS_AI, Prestation.BOURSES], membre),
+          calculateRefus: this.calculateRevenusRefusFn(membre),
+          eligibilites: eligibiliteGroup.findByPrestationEtMembre([Prestation.PC_FAM,
+                                                                   Prestation.AIDE_SOCIALE,
+                                                                   Prestation.PC_AVS_AI,
+                                                                   Prestation.BOURSES], membre),
           categorie: Categorie.SITUATION_PERSONELLE,
           subcategorie: Subcategorie.REVENUS
         }
@@ -146,50 +68,44 @@ export class RevenusQuestionService implements QuestionLoader {
   calculateRevenusRefusFn(membre: MembreFamille | Demandeur) {
     return (formData: FormData, eligibilites: Eligibilite[]): EligibiliteRefusee[] => {
       const refus: EligibiliteRefusee[] = [];
-      const revenusConjointAnswer = formData[`revenusConjoint_${membre.id}`] as CheckboxGroupAnswer;
-      const choices = revenusConjointAnswer.choices;
-
-      if (choices.some(choice => choice.value === 'AVS' || choice.value === 'AI')) {
-        this.createRefusByPrestation(
-          [Prestation.PC_FAM, Prestation.AIDE_SOCIALE], refus, eligibilites, 'revenusConjoint', membre
-        ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
-      }
-
-      if (choices.some(choice => choice.value === 'CHOMAGE' || choice.value === 'AVS_RETRAITE' || choice.value === 'AI_INVALIDITE')) {
-        this.createRefusByPrestation(
-          [Prestation.PC_FAM, Prestation.AIDE_SOCIALE], refus, eligibilites, 'revenusConjoint', membre
-        ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
-      }
-
-      return [];
-    };
-  }
-
-  calculateRevenusRefusEnfantsFn(membre: MembreFamille | Demandeur) {
-    return (formData: FormData, eligibilites: Eligibilite[]): EligibiliteRefusee[] => {
-      const refus: EligibiliteRefusee[] = [];
       const revenusEnfantsAnswer = formData[`revenusEnfants_${membre.id}`] as CheckboxGroupAnswer;
       const choices = revenusEnfantsAnswer.choices;
 
-      if (choices.some(choice => choice.value !== 'AVS' && choice.value !== 'AI')) {
-        this.createRefusByPrestation(
-          [Prestation.PC_AVS_AI], refus, eligibilites, 'revenusEnfants', membre
-        ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
-      }
+      if (membre.id !== 0) {
+        const membreFamille = membre as MembreFamille;
+        if (membreFamille.relation === Relation.ENFANT) {
+          if (choices.some(choice => choice.value !== 'AVS' && choice.value !== 'AI')) {
+            this.createRefusByPrestation(
+              [Prestation.PC_AVS_AI], refus, eligibilites, membre
+            ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
+          }
 
-      if (choices.some(choice => choice.value === 'CHOMAGE' || choice.value === 'AVS_RETRAITE' || choice.value === 'AI_INVALIDITE')) {
-        this.createRefusByPrestation(
-          [Prestation.BOURSES], refus, eligibilites, 'revenusEnfants', membre
-        ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
-      }
+          if (choices.some(choice => choice.value === 'CHOMAGE' || choice.value === 'AVS_RETRAITE' || choice.value === 'AI_INVALIDITE')) {
+            this.createRefusByPrestation(
+              [Prestation.BOURSES], refus, eligibilites, membre
+            ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
+          }
+        } else {
+          if (choices.some(choice => choice.value === 'AVS' || choice.value === 'AI')) {
+            this.createRefusByPrestation(
+              [Prestation.PC_FAM, Prestation.AIDE_SOCIALE], refus, eligibilites, membre
+            ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
+          }
 
+          if (choices.some(choice => choice.value === 'CHOMAGE' || choice.value === 'AVS_RETRAITE' || choice.value === 'AI_INVALIDITE')) {
+            this.createRefusByPrestation(
+              [Prestation.PC_FAM, Prestation.AIDE_SOCIALE], refus, eligibilites, membre
+            ).forEach(eligibiliteRefusee => refus.push(eligibiliteRefusee));
+          }
+        }
+      }
       return [];
     };
   }
 
   private createRefusByPrestation(prestations: Prestation[],
                                   refus: EligibiliteRefusee[],
-                                  eligibilites: Eligibilite[], key, membre: MembreFamille | Demandeur) {
+                                  eligibilites: Eligibilite[], membre: MembreFamille | Demandeur) {
     const translateParams = {who: membre.id === 0 ? 'me' : 'them', membre: membre.prenom};
     const eligibiliteGroup = new EligibiliteGroup(eligibilites);
 
@@ -198,14 +114,35 @@ export class RevenusQuestionService implements QuestionLoader {
         refus.push({
           eligibilite: eligibilite,
           motif: {
-            key: `question.${key}.motifRefus.${eligibilite.prestation}`,
+            key: `questionrevenus.motifRefus.${eligibilite.prestation}`,
             parameters: translateParams
           }
         });
       });
     });
-
     return refus;
+  }
+
+  private refusOptions(translateParams) {
+    return [
+      {value: TypeRevenus.EMPLOI, label: {key: `question.revenus.options.${TypeRevenus.EMPLOI}`, parameters: translateParams}},
+      {value: TypeRevenus.CHOMAGE, label: {key: `question.revenus.options.${TypeRevenus.CHOMAGE}`, parameters: translateParams}},
+      {
+          label: {key: 'question.revenus.AVS'}, options: [
+          {value: TypeRevenus.AVS_RETRAITE, label: {key: `question.revenus.options.${TypeRevenus.AVS_RETRAITE}`, parameters: translateParams}},
+          {value: TypeRevenus.AVS_VEUF, label: {key: `question.revenus.options.${TypeRevenus.AVS_VEUF}`, parameters: translateParams}},
+          {value: TypeRevenus.AVS_ENFANT, label: {key: `question.revenus.options.${TypeRevenus.AVS_ENFANT}`, parameters: translateParams}}
+        ]
+      },
+
+      {
+        label: {key: 'question.revenus.AI'}, options: [
+          {value: TypeRevenus.AI_ENFANT, label: {key: `question.revenus.options.${TypeRevenus.AI_ENFANT}`, parameters: translateParams}},
+          {value: TypeRevenus.AI_INVALIDITE, label: {key: `question.revenus.options.${TypeRevenus.AI_INVALIDITE}`, parameters: translateParams}}
+        ]
+      },
+      {value: TypeRevenus.APG, label: {key: `question.revenus.options.${TypeRevenus.APG}`, parameters: translateParams}, help: true}
+    ] as (QuestionOption<string> | CheckboxGroup)[];
   }
 
       /*
