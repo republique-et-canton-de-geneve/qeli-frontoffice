4e9327d23dc416695c9504f9cdf1096e758ca41c
ouillonke <ouillon.ko.kevin@etat.ge.ch>
2020-05-07 13:46:00 +0200

    [QELI-127]: review

--- qeli-frontoffice-ihm/src/app/app.component.ts
+++ qeli-frontoffice-ihm/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NgcCookieConsentService } from 'ngx-cookieconsent';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +17,25 @@ export class AppComponent implements OnInit {
     this.translate.addLangs(["fr"]);
     this.translate.setDefaultLang("fr");
     this.translate.use("fr");
+
+    this.translate
+        .get(['common.cookie.header', 'common.cookie.message', 'common.cookie.dismiss', 'common.cookie.allow', 'common.cookie.deny', 'common.cookie.link', 'common.cookie.policy'])
+        .subscribe(data => {
+
+          this.ccService.getConfig().content = this.ccService.getConfig().content || {} ;
+          // Override default messages with the translated ones
+          this.ccService.getConfig().content.header = data['common.cookie.header'];
+          this.ccService.getConfig().content.message = data['common.cookie.message'];
+          this.ccService.getConfig().content.dismiss = data['common.cookie.dismiss'];
+          this.ccService.getConfig().content.allow = data['common.cookie.allow'];
+          this.ccService.getConfig().content.deny = data['common.cookie.deny'];
+          this.ccService.getConfig().content.link = data['common.cookie.link'];
+          this.ccService.getConfig().content.policy = data['common.cookie.policy'];
+          this.ccService.getConfig().content.href = data['common.cookie.href'];
+
+          this.ccService.destroy(); // remove previous cookie bar (with default messages)
+          this.ccService.init(this.ccService.getConfig()); // update config with translated messages
+        });
 
   }
 }
