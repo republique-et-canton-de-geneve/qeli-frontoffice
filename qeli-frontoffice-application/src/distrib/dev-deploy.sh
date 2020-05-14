8dce11484955e14a775db95efc52958ade09b131
Josep Boix Requesens <josep.boixrequesens@etat.ge.ch>
2020-05-14 12:05:40 +0200

    [QELI-228] Fix pour le deploiement automatique en dev

--- qeli-frontoffice-application/src/distrib/dev-deploy.sh
+++ qeli-frontoffice-application/src/distrib/dev-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd ***REMOVED***/
-ln -f -s ../livraison/`ls -rt livraison/ | tail -n1` webapps/socialqeli_pub.war
+ln -f -s ../livraison/`ls -t livraison/ | tail -n1` webapps/socialqeli_pub.war
 sh ./etc/tomcat.bash restart
