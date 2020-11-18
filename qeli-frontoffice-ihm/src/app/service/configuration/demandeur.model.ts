f9577e918f5b7100dc382ace75dbd40afbd478f5
Wujkowski Guillaume (DI) <WUJKOWSKIG@ge-admin.ad.etat-ge.ch>
2020-11-18 10:39:01 +0100

    [QELI-251] suppression de la coche et ajout des valeurs dans la liste déroulante avec gestion required+disabled

--- qeli-frontoffice-ihm/src/app/service/configuration/demandeur.model.ts
+++ qeli-frontoffice-ihm/src/app/service/configuration/demandeur.model.ts
@@ -10,6 +10,8 @@ export enum Relation {
   PARTENAIRE_ENREGISTRE = 'PARTENAIRE_ENREGISTRE',
   CONCUBIN              = 'CONCUBIN',
   ENFANT                = 'ENFANT',
+  COLOCATAIRE           = 'COLOCATAIRE',
+  AUTRE                 = 'AUTRE',
 }
 
 /**
@@ -78,7 +80,6 @@ export abstract class Personne {
 export interface DemandeurSchema extends PersonneSchema {
   etatCivil: EtatCivil;
   membresFamille?: MembreFamilleSchema[];
-  autresMembres: boolean;
 }
 
 /**
@@ -96,16 +97,10 @@ export class Demandeur extends Personne {
    */
   membresFamille: MembreFamille[];
 
-  /**
-   * Si le demandeur habite avec d'autre personnes qui n'ont aucun rapport avec son éligibilité.
-   */
-  autresMembres: boolean;
-
   constructor(options: DemandeurSchema) {
     super(options);
     this.etatCivil = options.etatCivil;
     this.membresFamille = (options.membresFamille || []).map(membre => new MembreFamille(membre));
-    this.autresMembres = options.autresMembres;
   }
 
   toEligibilite(): Eligibilite[] {
@@ -215,5 +210,12 @@ export class MembreFamille extends Personne {
    */
   get isConcubin() {
     return this.relation === Relation.CONCUBIN;
+  }
+
+  /**
+   * Si les informations de saisie de ce membre sont optionnelles
+   */
+  get isOptional() {
+    return this.relation === Relation.COLOCATAIRE || this.relation === Relation.AUTRE;
   }
 }
