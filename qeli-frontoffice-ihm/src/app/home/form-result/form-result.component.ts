b63d0f22cb932e4cad626428d16c150faaac775b
Boix Requesens Josep (DI) <josep.boixrequesens@etat.ge.ch>
2020-12-01 09:51:25 +0100

    [QELI-222] Compléter le PDF avec des traductions

--- qeli-frontoffice-ihm/src/app/home/form-result/form-result.component.ts
+++ qeli-frontoffice-ihm/src/app/home/form-result/form-result.component.ts
@@ -3,14 +3,11 @@ import { QeliStateMachine } from '../../service/question/qeli-state.model';
 import { TranslateService } from '@ngx-translate/core';
 import { PDFGenerationService } from '../../service/pdf-generation.service';
 import { FormData } from '../../dynamic-question/model/question.model';
-import { Prestation } from '../../service/configuration/prestation.model';
-import { Eligibilite, EligibiliteRefusee } from '../../service/question/eligibilite.model';
 import * as FileSaver from 'file-saver';
 import { Result, ResultsByPrestation, resultsComparator } from './result-block/result.model';
-import { I18nString } from '../../core/i18n/i18nstring.model';
-import { AnswerUtils } from '../../service/question/answer-utils';
-import { TypeEnfant } from '../../service/question/enfants/type-enfant.model';
-import { Scolarite } from '../../service/question/formation/scolarite.model';
+import { FormResult } from '../../service/question/result.model';
+import { FormResultService } from '../../service/question/form-result.service';
+import { Demandeur } from '../../service/configuration/demandeur.model';
 
 @Component({
   selector: 'app-form-result',
@@ -20,17 +17,14 @@ import { Scolarite } from '../../service/question/formation/scolarite.model';
 })
 export class FormResultComponent {
   formData: FormData;
+  formResult: FormResult;
+  demandeur: Demandeur;
 
-  prestationEligibles: ResultsByPrestation[] = [];
-  prestationRefusees: ResultsByPrestation[] = [];
-  prestationDejaPercues: ResultsByPrestation[] = [];
   @Input() canSavePDF: boolean = true;
   @Input() disableDeepLink: boolean = false;
 
-  private eligibilites: Eligibilite[];
-  private eligibilitesRefusees: EligibiliteRefusee[];
-
   constructor(private translateService: TranslateService,
+              private formResultService: FormResultService,
               private pdfGenerationService: PDFGenerationService) {
 
   }
@@ -38,73 +32,16 @@ export class FormResultComponent {
   @Input()
   set qeliStateMachine(qeliStateMachine: QeliStateMachine) {
     const state = qeliStateMachine.state;
-
-    // TODO Create a service for the mapping
-
-    const conjointEnfantsPropres = state.demandeur.enfants.some(enfant =>
-      AnswerUtils.isEnfantType(state.formData, enfant.id, TypeEnfant.AUTRE_PARENT)
-    );
-
-    Object.values(Prestation).filter(prestation => prestation !== Prestation.SUBVENTION_HM).forEach(prestation => {
-      const results: Result[] = [];
-
-      qeliStateMachine.currentEligibilites.filter(
-        eligibilite => eligibilite.prestation === prestation
-      ).map(
-        eligibilite => ({membre: state.demandeur.findMembrebyId(eligibilite.membreId), eligible: true})
-      ).forEach(result => results.push(result));
-
-      state.eligibilitesRefusees.filter(eligibiliteRefusee =>
-        eligibiliteRefusee.eligibilite.prestation === prestation
-      ).map(eligibiliteRefusee => ({
-        membre: state.demandeur.findMembrebyId(eligibiliteRefusee.eligibilite.membreId),
-        eligible: false,
-        dejaPercue: eligibiliteRefusee.dejaPercue,
-        motifRefus: eligibiliteRefusee.motif,
-      })).forEach(result => {
-        if (result.motifRefus && ![Prestation.BOURSES, Prestation.SUBSIDES].includes(prestation)) {
-          const translateMotif = (i18n: I18nString) => this.translateService.instant(i18n.key, i18n.parameters);
-          const translatedResultMotif = translateMotif(result.motifRefus);
-          if (!results.some(r => r.motifRefus && translateMotif(r.motifRefus) === translatedResultMotif)) {
-            results.push(result);
-          }
-        } else {
-          results.push(result);
-        }
-        if (!result.eligible && prestation === Prestation.BOURSES) {
-          (result as Result).enfants11P = state.demandeur.enfants.filter(
-            enfant => AnswerUtils.isScolarite(state.formData, enfant.id, Scolarite.SCOLARITE_OBLIGATOIRE_11P)
-          );
-        }
-      });
-
-      results
-        .filter(result => result.membre.id === 0)
-        .forEach(result => result.conjointEnfantsPropres = conjointEnfantsPropres);
-
-      if (results.some(result => result.eligible)) {
-        this.prestationEligibles.push({
-          prestation: prestation,
-          results: results.sort(resultsComparator).reverse()
-        });
-      } else if (results.every(result => result.dejaPercue)) {
-        this.prestationDejaPercues.push({prestation: prestation, results: results});
-      } else {
-        this.prestationRefusees.push({prestation: prestation, results: results});
-      }
-    });
-
-    this.eligibilites = qeliStateMachine.currentEligibilites;
-    this.eligibilitesRefusees = state.eligibilitesRefusees;
-
+    this.formResult = this.formResultService.toFormResult(qeliStateMachine);
     this.formData = state.formData;
+    this.demandeur = state.demandeur;
   }
 
   generatePDF() {
     this.pdfGenerationService.generatePDF(
       this.formData,
-      this.eligibilites,
-      this.eligibilitesRefusees
+      this.formResult,
+      this.demandeur
     ).subscribe(response => {
       const blob = new Blob([(response)], {type: 'application/pdf'});
       FileSaver.saveAs(blob, 'recapitulatif-questionaire-eligibilite.pdf');
