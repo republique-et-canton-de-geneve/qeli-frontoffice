4276bbff35b9b22f45f33bab029f2c8e22fc5f41
Blanco Morales Pavel <pavel.blanco-morales@etat.ge.ch>
2020-12-01 19:06:01 +0100

    [QELI-269] déplacement des generateurs de message dans le bon package

--- qeli-frontoffice-ihm/src/app/service/question/message-evaluators/pcavsai.evaluator.ts
+++ qeli-frontoffice-ihm/src/app/service/question/message-evaluators/pcavsai.evaluator.ts
@@ -1,11 +1,12 @@
 import { MessageEvaluator, ResultsByPrestation, TypeEligibilite } from '../result.model';
-import { Prestation } from '../../../../service/configuration/prestation.model';
+import { Prestation } from '../../configuration/prestation.model';
 import { EvaluatorUtils } from '../evaluator-utils';
-import { I18nString } from '../../../../core/i18n/i18nstring.model';
+import { I18nString } from '../../../core/i18n/i18nstring.model';
+import { FormData } from '../../../dynamic-question/model/question.model';
 
 export class PcAvsAiEvaluator implements MessageEvaluator {
 
-  evaluate(type: TypeEligibilite, results: ResultsByPrestation): string | I18nString {
+  evaluate(type: TypeEligibilite, results: ResultsByPrestation, formData: FormData): string | I18nString {
     if (EvaluatorUtils.isPrestationRefusee(type) && EvaluatorUtils.hasConcubin(results)) {
       return {
         key: 'home.result.verifierConcubin',
--- qeli-frontoffice-ihm/src/app/service/question/message-evaluators/pcfam.evaluator.ts
+++ qeli-frontoffice-ihm/src/app/service/question/message-evaluators/pcfam.evaluator.ts
@@ -1,11 +1,12 @@
 import { MessageEvaluator, ResultsByPrestation, TypeEligibilite } from '../result.model';
-import { I18nString } from '../../../../core/i18n/i18nstring.model';
-import { Prestation } from '../../../../service/configuration/prestation.model';
+import { I18nString } from '../../../core/i18n/i18nstring.model';
+import { Prestation } from '../../configuration/prestation.model';
 import { EvaluatorUtils } from '../evaluator-utils';
+import { FormData } from '../../../dynamic-question/model/question.model';
 
 export class PcFamEvaluator implements MessageEvaluator {
 
-  evaluate(type: TypeEligibilite, results: ResultsByPrestation): string | I18nString {
+  evaluate(type: TypeEligibilite, results: ResultsByPrestation, formData: FormData): string | I18nString {
     if (EvaluatorUtils.isPrestationEligible(type) && !EvaluatorUtils.isPrestationIndividuel(results)) {
       return 'home.result.prestation.PC_FAM.explication';
     }
