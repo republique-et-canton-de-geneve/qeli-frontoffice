97bcd4356f1f94c3f08234445493170c6ac1b2a3
Boix Requesens Josep (DI) <josep.boixrequesens@etat.ge.ch>
2021-12-15 10:30:09 +0100

    [QELI-224] Ajout des copyright et du thème état

--- qeli-frontoffice-ihm/src/app/dynamic-question/model/question-visitor.model.ts
+++ qeli-frontoffice-ihm/src/app/dynamic-question/model/question-visitor.model.ts
@@ -1,3 +1,22 @@
+/*
+ * qeli-frontoffice
+ *
+ * Copyright (C) 2019-2021 Republique et canton de Geneve
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { CheckboxGroupQuestion } from '../checkbox-group-question/checkbox-group-question.model';
 import { DateQuestion } from '../date-question/date-question.model';
 import { DropdownQuestion } from '../dropdown-question/dropdown-question.model';
